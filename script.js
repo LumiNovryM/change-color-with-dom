5314ce259d5e398e0897c90563e865884929c5ef
LumiNovryM <luminovrymekel@gmail.com>
2022-10-07 07:41:49 +0700

    Push

---
+++ script.js
@@ -0,0 +1,58 @@
+const tUbahWarna = document.getElementById('tUbahWarna');
+tUbahWarna.onclick = function() {
+    // document.body.style.backgroundColor = 'lightblue';
+    // document.body.setAttribute('class', 'biru-muda');
+    document.body.classList.toggle('biru-muda');
+}
+
+
+
+const tAcakWarna = document.createElement('button');
+const teksTombol = document.createTextNode('Acak Warna');
+tAcakWarna.setAttribute('type', 'button');
+tAcakWarna.appendChild(teksTombol)
+
+tUbahWarna.after(tAcakWarna);
+
+
+tAcakWarna.addEventListener('click', function() {
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')'
+});
+
+
+const sMerah = document.querySelector('input[name=sMerah]')
+const sHijau = document.querySelector('input[name=sHijau]')
+const sBiru = document.querySelector('input[name=sBiru]')
+
+
+sMerah.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')'
+});
+sHijau.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')'
+});
+sBiru.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')'
+});
+
+
+document.body.addEventListener('mousemove', function() {
+    // posisi mouse
+    // Event.clientX
+    // Ukuran Browser
+    const xPos = Math.round((event.clientX / window.innerWidth) * 255)
+    const yPos = Math.round((event.clientY / window.innerHeight) * 255)
+    document.body.style.backgroundColor = 'rgb('+ xPos +','+ yPos +', 100)'
+});
